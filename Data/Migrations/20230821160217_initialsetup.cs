a06516929bbb4a1bbdaa807622116f7adcb7dfb2
Vinura <vinuraperer2@gmail.com>
2023-08-22 11:15:14 +0530

    First Commit

---
+++ Data/Migrations/20230821160217_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_App.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Note",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NoteTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    NoteDesc = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Note", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Note");
+        }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_App.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        public string NoteTitle { get; set; }
+
+        public string NoteDesc { get; set; }
+
+        public Note()
+        {
+
+        }
+
+    }
+
+    
+}
